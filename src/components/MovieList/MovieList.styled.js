734a818ba6d9b09ff03dddeb267d5f3f7d991423
rilervi <valeriazv2018@gmail.com>
2025-07-12 11:01:25 +0000

    Deploying to gh-pages from @ rilervi/FilmFounder1@e5eee1b8781256fbd115d28456d47ad288e39186 🚀

--- src/components/MovieList/MovieList.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledList = styled.ul`
-  display: flex;
-  gap: 24px;
-  flex-wrap: wrap;
-  justify-content: center;
-  margin-bottom: 24px;
-`;
